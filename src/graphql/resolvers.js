96dcab107afef385fc8ab7d91bc5dcfbd1333b4c
Samvel Pahalyants <spahalyants@gmail.com>
2026-04-03 12:01:30 -0600

    add generatedId.js and update resolvers.js

---
+++ src/graphql/resolvers.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+import User from "../models/User.js";
+import Comment from "../models/Comment.js";
+import generateId from "../utils/generatedId.js";
+
+module.exports = {
+    Query: {
+
+    },
+    Mutation: {
+        async userCreate(_, {userInput: {firstName, lastName}}) {
+            const userId = generateId()
+            const createdUser = new User({
+                _id: userId,
+                firstName: firstName,
+                lastName: lastName,
+            });
+
+            const res = await createdUser.save();
+
+            return {
+                id: res.id,
+                ...res._doc
+            }
+        },
+
+    }
+}
